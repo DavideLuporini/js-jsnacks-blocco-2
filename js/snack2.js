301ffdb8cd8f1af375343c71987f7db3bcc7c0fe
illupovero <davideluporini1996@gmail.com>
2022-01-13 17:39:58 +0100

    used while in snack2 , fixed snack 1

--- js/snack2.js
+++ js/snack2.js
@@ -1,12 +1,20 @@
 // Inserisci un numero, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
-let number = parseInt(prompt('inserisci un numero'))
+const number = parseInt(prompt('Inserisci un numero'));
+let solution = 0;
+let i = 0;
 
+while (i < 1) {
+    if (number % 2 == 0) {
+        console.log(number);
+        solution = number;
+    } else {
+        console.log(number + 1);
+        solution = number + 1;
+    }
+    i++;
 
-if (number % 2 !== 0) {
-    number += 1
-    console.log(number)
-} else {
-    console.log(number)
 }
+
+console.log(solution);
--- js/snack5.js
+++ js/snack5.js
@@ -4,19 +4,13 @@
 
 
 
-const array1 = [10, 15, 22, 33, 73];
-const array2 = [11, 23, 56, 78, 99, 40, 35, 26];
+let array1 = [10, 15, 22, 33, 73];
+let array2 = [11, 23, 56, 78, 99, 40, 35, 26];
 
-let difference = array1.length - array2.length
-
-console.log(array1.length)
-console.log(array2.length)
 
 while (array1.length < array2.lenght) {
-    let num = Math.floor(Math.random() * 100);
-
-
-    array1.push(num);
+    let addNum = Math.floor(Math.random() * 10);
+    array1.push(addNum);
 }
 
 
